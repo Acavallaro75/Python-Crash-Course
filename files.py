26640e790ce3bdadc9610debf756facc8a23f094
Andrew Cavallaro <47667938+Acavallaro75@users.noreply.github.com>
2020-03-19 13:10:22 -0400

    Complete

--- files.py
+++ files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files #
+
+# Open a file #
+myFile = open('test.txt', 'w')
+
+# Get information about the file #
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Writing to a file #
+myFile.write('I love Python')
+myFile.write(' and JavaScript.')
+myFile.close
+
+# Append to a file #
+myFile = open('test.txt', 'a')
+myFile.write(' I also like Java.')
+myFile.close
+
+# Reading from a file #
+myFile = open('test.txt', 'r+')
+text = myFile.read(100)
+print(text)
--- py_json.py
+++ py_json.py
@@ -1,1 +1,24 @@
-# JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+'''
+    - JSON is commonly used with data API'S
+    - Here how we can parse JSON into a Python Dictionary
+'''
+
+# Import the JSON core module #
+import json
+
+# Sample JSON #
+userJSON = '{"firstName": "Andrew", "lastName": "Cavallaro", "age": 29}'
+
+# Parse JSON to Python Dictionary #
+user = json.loads(userJSON)
+
+print(user)
+print(user['firstName'])
+
+# Creating a Dictionary #
+car = {'make': 'Ford', 'model': 'Mustang', 'year': 2020}
+
+# Parsing Python Dictionary into JSON #
+carJSON = json.dumps(car)
+
+print(carJSON)
